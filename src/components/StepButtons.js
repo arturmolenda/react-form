34a609423c6bdcc89b0253edf101b4d82ef2f885
arturmolenda <apmolenda@gmail.com>
2021-03-19 18:25:08 +0100

    StepButtons handling step change

---
+++ src/components/StepButtons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { useDispatch } from 'react-redux';
+import { setStep, updateUserData } from '../redux/actions/Actions';
+
+import { Button } from '@material-ui/core';
+import Styled from 'styled-components';
+
+const Container = Styled.div`
+&& {
+    display: flex;
+    margin-top: 16px
+}
+`;
+
+const StepButtons = ({ step, userData }) => {
+  const dispatch = useDispatch();
+
+  const goBackHandle = () => dispatch(setStep(step - 1));
+  const nextHandle = () => {
+    dispatch(setStep(step + 1));
+    dispatch(updateUserData(userData));
+  };
+
+  return (
+    <Container>
+      {step !== 0 && (
+        <Button onClick={goBackHandle} color='secondary'>
+          Wstecz
+        </Button>
+      )}
+      {step !== 2 && (
+        <Button onClick={nextHandle} color='primary' variant='contained'>
+          Dalej
+        </Button>
+      )}
+    </Container>
+  );
+};
+
+export default StepButtons;
